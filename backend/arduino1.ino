70e30842f92f73ac2aaff07305de8510a84e6e51
aleks_wilberg <aleksanderwilberg@gmail.com>
2022-09-08 13:22:38 +0200

    added arduinofile

---
+++ backend/arduino1.ino
@@ -0,0 +1,78 @@
+int ButtonValue = 0;
+int ButtonValue1 = 0;
+int ButtonValue2 = 0;
+int ButtonValue3 = 0;
+
+int Button = 2;
+int Button1 = 3;
+int Button2 = 4;
+int Button3 = 5;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(Button, INPUT);
+  pinMode(Button1, INPUT);
+  pinMode(Button2, INPUT);
+  pinMode(Button3, INPUT);
+
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  ButtonValue = digitalRead(Button);
+  ButtonValue1 = digitalRead(Button1);
+  ButtonValue2 = digitalRead(Button2);
+  ButtonValue3 = digitalRead(Button3);
+
+    delay(500);
+    
+    //Button 0
+  if(ButtonValue != 0){
+  //if button is pressed then turn the led on
+    Serial.print(1);
+    
+  }
+  else{
+    //else the button is not pressed, turn it of
+    Serial.print(0);
+  }
+  
+  //Button 1
+  if(ButtonValue1 != 0){
+  //if button is pressed then turn the led on
+    Serial.print(2);
+    
+  }
+  else{
+    
+    //else the button is not pressed, turn it of
+    Serial.print(0);
+
+  }
+    //Button 2
+  if(ButtonValue2 != 0){
+  //if button is pressed then turn the led on
+    Serial.print(3);
+    
+  }
+  else{
+    
+    //else the button is not pressed, turn it of
+    Serial.print(0);
+  }
+
+
+  //Button 3
+  if(ButtonValue3 != 0){
+  //if button is pressed then turn the led on
+    Serial.print(4);
+    
+  }
+  else{
+    //else the button is not pressed, turn it of
+    Serial.print(0);
+  }
+
+  Serial.print('\n');
+}
