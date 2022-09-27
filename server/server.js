8754c6993867e53b3423a97ea0693fdecb21e6a2
TheSkibb <kfskibrek@icloud.com>
2022-09-27 11:16:33 +0200

    added nodemon and npm run dev script

--- server/server.js
+++ server/server.js
@@ -1,13 +1,18 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 const port = 3000
 
+var retults = [] 
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
 
+//app.use(express.static(__dirname + 
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+      res.sendFile(path.join(__dirname, '/frontend/index.html'))
+      res.sendFile(path.join(__dirname, '/frontend/styles/styles.css'))
 })
 
 app.listen(port, () => {
@@ -17,5 +22,12 @@ app.listen(port, () => {
 app.post('/seats', async (req, res) => {
   console.log('post request received');
   await console.log(req.body)
+  results.push(req.body)
   res.send('received')
 })
+
+app.get('/seats', async (req, res) =>{
+  res.sendFile('./frontend/iframehtml.html')
+})
+
+
