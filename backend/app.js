19c9111f468fa463b5a3a74c7a786d53e6ccdc0c
TheSkibb <kfskibrek@icloud.com>
2022-09-29 13:35:01 +0200

    removed process.env for now, should be^Ceintroduced later

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 
 const { SerialPort } = require('serialport')
 const { ReadlineParser } = require('@serialport/parser-readline')
-const port = new SerialPort({ path: PROCESS.ENV.ARDUINO_PORT, baudRate: 9600 })
+const port = new SerialPort({ path: '/dev/ttyACM0', baudRate: 9600 })
 
 const parser = port.pipe(new ReadlineParser({ delimiter: '\n' }))
 parser.on('data', )
@@ -10,4 +10,3 @@ parser.on('data', )
 function sendLog(data){
   console.log(data)
 }
-
