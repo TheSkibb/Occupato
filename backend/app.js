41169d87921463ebbca7fdc41b05910f25851935
TheSkibb <kfskibrek@icloud.com>
2022-09-29 13:40:36 +0200

    testing async log and converting to array, also introducing the post

--- backend/app.js
+++ backend/app.js
@@ -5,8 +5,22 @@ const { ReadlineParser } = require('@serialport/parser-readline')
 const port = new SerialPort({ path: '/dev/ttyACM0', baudRate: 9600 })
 
 const parser = port.pipe(new ReadlineParser({ delimiter: '\n' }))
-parser.on('data', )
+parser.on('data', await sendLog)
 
-function sendLog(data){
-  console.log(data)
+async function sendLog(data){
+  data = Array.from(data)
+}
+
+async function postSeats(inputSeats){
+  await axios.post('http://localhost:3000/seats', 
+  {
+    seats: inputSeats
+  }
+  )
+  .then(function (response) {
+    //console.log(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 }
