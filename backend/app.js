02de7a0b243676509707d415fce2edc40eb229d7
TheSkibb <kfskibrek@icloud.com>
2022-09-29 13:15:46 +0200

    separated arduino port into separat .env file for easy configuration

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 
 const { SerialPort } = require('serialport')
 const { ReadlineParser } = require('@serialport/parser-readline')
-const port = new SerialPort({ path: '/dev/ttyACM1', baudRate: 9600 })
+const port = new SerialPort({ path: PROCESS.ENV.ARDUINO_PORT, baudRate: 9600 })
 
 const parser = port.pipe(new ReadlineParser({ delimiter: '\n' }))
 parser.on('data', )
